06449aedbff7897c6d23c61120a595e90d09eada
adhiraj135 <adhiraj.singh483@gmail.com>
2023-06-14 04:21:38 -0700

    final commit

--- src/model_training.py
+++ src/model_training.py
@@ -4,12 +4,7 @@ from src.logger import log
 from src.Database.database_builder import database
 from src.Modelling.model_selection import modelling
 from src.utils import utils
-from cassandra.auth import PlainTextAuthProvider
-from cassandra.cluster import Cluster
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score
-import pandas as pd
 
 class model_training:
     def __init__(self):
@@ -47,19 +42,16 @@ class model_training:
            X=df.drop(columns=['credit_risk'])
            Y=df['credit_risk']
            x,y=self.preprocessor.over_sampling_operation(features=X,label=Y)
-           print(x,y)
-           #x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=42)
+           #print(x,y)
+           x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=42)
            #print(x_train,y_train,x_test,y_test)
-           #grad=GradientBoostingClassifier()
-           #grad.fit(x_train,y_train)
-           #p=grad.predict(x_test)
-           #print(accuracy_score(y_test,p))
-           #self.log.log(self.file_object, "Data Preprocessing Completed!!")
-           #self.log.log(self.file_object, "modelling using ML algorithms started!!")
-           #model_name,model_object=self.model.best_model_selection(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test)
-           #self.log.log(self.file_object, "modelling Completed, Best model is selected i.e %s"%model_name)
-           #self.utils.model_saver(model_name=model_name,model=model_object)
-           #self.log.log(self.file_object, "model training Completed Successfully!!")
+
+           self.log.log(self.file_object, "Data Preprocessing Completed!!")
+           self.log.log(self.file_object, "modelling using ML algorithms started!!")
+           model_name,model_object=self.model.best_model_selection(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test)
+           self.log.log(self.file_object, "modelling Completed, Best model is selected i.e %s"%model_name)
+           self.utils.model_saver(model_name=model_name,model=model_object)
+           self.log.log(self.file_object, "model training Completed Successfully!!")
 
         except Exception as e:
             self.log.log(self.file_object,"exception occurred while model training : %s"%e)
